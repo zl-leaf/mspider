df55bd8bb6fa0490f8477d4080ab234534e803c7
叶志良 <leaf@yezhiliangdeMacBook-Air.local>
2016-09-17 18:46:48 +0800

    增加mspider.go作为调用入口

---
+++ mspider.go
@@ -0,0 +1,61 @@
+package mspider
+import(
+    "os"
+    "time"
+    "github.com/zl-leaf/mspider/config"
+    "github.com/zl-leaf/mspider/engine"
+    "github.com/zl-leaf/mspider/scheduler"
+    "github.com/zl-leaf/mspider/downloader"
+    "github.com/zl-leaf/mspider/spider"
+    "github.com/zl-leaf/mspider/logger"
+)
+
+type MSpider struct {
+    mEngine *engine.Engine
+}
+
+func New() (m *MSpider, err error) {
+    m = &MSpider{}
+    m.init()
+    return
+}
+
+func (this *MSpider) init() {
+    this.mEngine = &engine.Engine{}
+    this.mEngine.Init()
+}
+
+func (this *MSpider) Load(mConfig *config.Config) error {
+    s,_ := scheduler.New()
+    if mConfig.SpiderInterval > 0 {
+        s.Interval = mConfig.SpiderInterval
+    } else {
+        s.Interval = int(time.Second)
+    }
+    this.mEngine.SetScheduler(s)
+
+    for i := 0; i < mConfig.DownloaderNum; i++ {
+        d,_ := downloader.New("")
+        this.mEngine.AddDownloader(d)
+    }
+
+    if mConfig.LogPath != "" {
+        logger.SetLogPath(mConfig.LogPath)
+    } else {
+        logger.SetLogPath("./")
+    }
+    return nil
+}
+
+func (this *MSpider) RegisterSpider(s *spider.Spider) {
+    this.mEngine.AddSpider(s)
+}
+
+func (this *MSpider)Start() {
+    this.mEngine.Start()
+}
+
+func (this *MSpider)Stop() {
+    this.mEngine.Stop()
+    os.Exit(0)
+}
