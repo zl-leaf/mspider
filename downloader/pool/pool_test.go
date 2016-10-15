d922152c202bf7d3d283eeed9368f9f6efc5d7eb
叶志良 <leaf@yezhiliangdeMacBook-Air.local>
2016-10-15 11:43:57 +0800

    downloader pool增加数量限制

--- downloader/pool/pool_test.go
+++ downloader/pool/pool_test.go
@@ -10,7 +10,7 @@ func TestPool(t *testing.T) {
     db,_ := downloader.New()
     idb := db.ID
 
-    p := New()
+    p := New(0)
     p.Put(da)
     p.Put(db)
 
@@ -19,5 +19,10 @@ func TestPool(t *testing.T) {
 
     if a.ID != ida || b.ID != idb {
         t.Errorf("should got ida:%s and idb:%s, but got ida:%s and idb%s", ida, idb, a.ID, b.ID)
+    }
+
+    p.Total = 1
+    if !p.Put(a) || p.Put(b) {
+        t.Error("pool shoul only put one downloader success, but it is not now")
     }
 }
